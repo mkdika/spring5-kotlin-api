67b616d75f73500c8faa4dac0a5658cc09718a82
Maikel Chandika <mkdika@gmail.com>
2018-09-24 20:20:31 +0700

    add model class demo.

---
+++ src/main/kotlin/com/mkdika/springkotlinapi/model/Customer.kt
@@ -0,0 +1,8 @@
+package com.mkdika.springkotlinapi.model
+
+data class Customer (
+        var id: Int,
+        var firstName: String,
+        var lastName: String,
+        var balance: Double
+)
